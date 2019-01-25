d79cf8050d8d48194f224497f62927043d2413dc
marczaku <mzaku@hotmail.de>
2019-01-25 18:35:32 +0100

    some garbage improvements

---
+++ Entitas/Entitas/EntityIndex/HashSetPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Entitas {
+
+    public static class HashSetPool<T> {
+		public static readonly HashSet<T> instance = new HashSet<T>();
+		static Queue<HashSet<T>> pool = new Queue<HashSet<T>>();
+
+		public static HashSet<T> GetInstance() {
+			if(pool.Count > 0)
+				return pool.Dequeue();
+			else
+				return new HashSet<T>();
+		}
+
+		public static void ReturnInstance(HashSet<T> instance) {
+			pool.Enqueue(instance);
+		}
+	}
+
+	public static class ResizingArray<T> {
+		static T[] instance = new T[12];
+
+		public static T[] GetInstance(int size) {
+			var length = instance.Length;
+			while (length < size) {
+				length *= 2;
+				if (length >= size)
+					System.Array.Resize<T>(ref instance, length);
+			}
+			return instance;
+		}
+	}
+}
